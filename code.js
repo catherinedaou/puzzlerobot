26a327c2438f64ade55f87e595fdbbd32033a114
Catherine Daou <catherine.daou@gmail.com>
2021-12-13 13:00:53 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,36 @@
+onEvent("eye1", "click", function( ) {
+  setPosition("eye1", 75, 0);
+});
+onEvent("eye2", "click", function( ) {
+  setPosition("eye2", 125, 0);
+});
+onEvent("Nose3", "click", function( ) {
+  setPosition("Nose3", 75, 95);
+});
+onEvent("Mouth4", "click", function( ) {
+  setPosition("Mouth4", 75, 135);
+});
+onEvent("image5", "click", function( ) {
+  setPosition("image5", 75, 140);
+});
+onEvent("ha6", "click", function( ) {
+  setPosition("ha6", 0, 190);
+});
+onEvent("nd7", "click", function( ) {
+  setPosition("nd7", 175, 195);
+});
+onEvent("st8", "click", function( ) {
+  setPosition("st8", 75, 180);
+});
+onEvent("om9", "click", function( ) {
+  setPosition("om9", 135, 180);
+});
+onEvent("k10", "click", function( ) {
+  setPosition("k10", 80, 275);
+});
+onEvent("le11", "click", function( ) {
+  setPosition("le11", 80, 315);
+});
+onEvent("gs12", "click", function( ) {
+  setPosition("gs12", 140, 315);
+});
